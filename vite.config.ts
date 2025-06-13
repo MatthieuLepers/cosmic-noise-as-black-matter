ace846f3aeb1894556771c2744dfb48f07f029b2
Matthieu LEPERS <aireayquaza@hotmail.fr>
2025-06-13 23:08:15 +0200

    :bug: change Github Pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/gravity-simulation/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/cosmic-noise-as-black-matter/' : '/',
   // server: {
   //   https: {
   //     key: readFileSync('./api/src/cert/key.pem'),
